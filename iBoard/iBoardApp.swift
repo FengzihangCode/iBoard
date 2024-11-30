01916ccecc508f82ceaf78d80cc668e18f36afaf
DannyFeng <FengzihangCode@outlook.com>
2024-11-30 10:30:23 +0800

    Initial Commit

---
+++ iBoard/iBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBoardApp.swift
+//  iBoard
+//
+//  Created by feng on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct iBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
